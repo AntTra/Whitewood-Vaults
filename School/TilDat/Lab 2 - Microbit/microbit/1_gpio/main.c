a85f2f6bd37281abecf9b1787e1e2941ce89d4c8
Anton Tran <antontr@stud.ntnu.no>
2023-04-21 03:19:22 +0200

    update

--- School/TilDat/Lab 2 - Microbit/microbit/1_gpio/main.c
+++ School/TilDat/Lab 2 - Microbit/microbit/1_gpio/main.c
@@ -1,68 +1,103 @@
 #include <stdint.h>
 
-#define GPIO0 ((NRF_GPIO_REGS0*)0x50000000) //gpio 0 base adress
-#define GPIO1 ((NRF_GPIO_REGS1*)0x50000300) //gpio 1 base adress
+#define GPIO0 ((NRF_GPIO_REGS0 *)0x50000000) // gpio 0 base adress
+#define GPIO1 ((NRF_GPIO_REGS1 *)0x50000300) // gpio 1 base adress
 
 typedef struct {
-	volatile uint32_t RESERVED0[321];
-	volatile uint32_t OUT;
-	volatile uint32_t OUTSET;
-	volatile uint32_t OUTCLR;
-	volatile uint32_t IN;
-	volatile uint32_t DIR;
-	volatile uint32_t DIRSET;
-	volatile uint32_t DIRCLR;
-	volatile uint32_t LATCH;
-	volatile uint32_t DETECTMODE;
-	volatile uint32_t RESERVED1_0[__RESERVED1_0_SIZE__];
-	volatile uint32_t PIN_CNF[32];
+  volatile uint32_t RESERVED0[321];
+  volatile uint32_t OUT;
+  volatile uint32_t OUTSET;
+  volatile uint32_t OUTCLR;
+  volatile uint32_t IN;
+  volatile uint32_t DIR;
+  volatile uint32_t DIRSET;
+  volatile uint32_t DIRCLR;
+  volatile uint32_t LATCH;
+  volatile uint32_t DETECTMODE;
+  volatile uint32_t RESERVED1_0[118];
+  volatile uint32_t PIN_CNF[32];
 } NRF_GPIO_REGS0;
 
 typedef struct {
-	volatile uint32_t RESERVED0[321];
-	volatile uint32_t OUT;
-	volatile uint32_t OUTSET;
-	volatile uint32_t OUTCLR;
-	volatile uint32_t IN;
-	volatile uint32_t DIR;
-	volatile uint32_t DIRSET;
-	volatile uint32_t DIRCLR;
-	volatile uint32_t LATCH;
-	volatile uint32_t DETECTMODE;
-	volatile uint32_t RESERVED1_1[__RESERVED1_1_SIZE__];
-	volatile uint32_t PIN_CNF[10];
+  volatile uint32_t RESERVED0[321];
+  volatile uint32_t OUT;
+  volatile uint32_t OUTSET;
+  volatile uint32_t OUTCLR;
+  volatile uint32_t IN;
+  volatile uint32_t DIR;
+  volatile uint32_t DIRSET;
+  volatile uint32_t DIRCLR;
+  volatile uint32_t LATCH;
+  volatile uint32_t DETECTMODE;
+  volatile uint32_t RESERVED1_1[128];
+  volatile uint32_t PIN_CNF[10];
 } NRF_GPIO_REGS1;
 
-int main(){
+int ubit_button_press_a() { return (!(GPIO0->IN & (1 << 14))); }
 
-	// Configure leds (dere må sjekke selv hvilken GPIO modul de ulike knappene tilhører)
-	__GPIOX__->PIN_CNF[21] = 1; //Row 1
-	__GPIOX__->PIN_CNF[22] = 1; //Row 2
-	__GPIOX__->PIN_CNF[15] = 1; //Row 3
-	__GPIOX__->PIN_CNF[24] = 1; //Row 4
-	__GPIOX__->PIN_CNF[19] = 1; //Row 5
+int ubit_button_press_b() { return (!(GPIO1->IN & (1 << 14))); }
 
-	__GPIOX__->PIN_CNF[28] = 1; //Col 1
-	__GPIOX__->PIN_CNF[11] = 1; //Col 2
-	__GPIOX__->PIN_CNF[31] = 1; //Col 3
-	__GPIOX__->PIN_CNF[5] = 1;  //Col 4
-	__GPIOX__->PIN_CNF[30] = 1; //Col 5 
-	
-	// Configure buttons (dere må sjekke selv hvilken GPIO modul de ulike knappene tilhører)
-	__GPIOX__->PIN_CNF[__BUTTON_A_PIN__] = 0; // button A 
-	__GPIOX__->PIN_CNF[__BUTTON_B_PIN__] = 0; // button B
-	
-	int sleep = 0;
-	while(1){
+int main() {
 
-		/* Check if button B is pressed;
-		 * turn on LED matrix if it is. */
+  // Configure leds (dere må sjekke selv hvilken GPIO modul de ulike knappene
+  // tilhører)
+  GPIO0->PIN_CNF[21] = 1; // Row 1
+  GPIO0->PIN_CNF[22] = 1; // Row 2
+  GPIO0->PIN_CNF[15] = 1; // Row 3
+  GPIO0->PIN_CNF[24] = 1; // Row 4
+  GPIO0->PIN_CNF[19] = 1; // Row 5
 
-		/* Check if button A is pressed;
-		 * turn off LED matrix if it is. */
+  GPIO0->PIN_CNF[28] = 1; // Col 1
+  GPIO0->PIN_CNF[11] = 1; // Col 2
+  GPIO0->PIN_CNF[31] = 1; // Col 3
+  GPIO1->PIN_CNF[5] = 1;  // Col 4
+  GPIO0->PIN_CNF[30] = 1; // Col 5
 
-		sleep = 10000;
-		while(--sleep);
-	}
-	return 0;
+  // Configure buttons (dere må sjekke selv hvilken GPIO modul de ulike knappene
+  // tilhører)
+  GPIO0->PIN_CNF[14] = 0; // button A
+  GPIO0->PIN_CNF[23] = 0; // button B
+
+  int sleep = 0;
+  while (1) {
+
+    /* Check if button B is pressed;
+     * turn on LED matrix if it is. */
+
+    if (ubit_button_press_b()) {
+      GPIO0->PIN_CNF[21] = 1; // Row 1
+      GPIO0->PIN_CNF[22] = 1; // Row 2
+      GPIO0->PIN_CNF[15] = 1; // Row 3
+      GPIO0->PIN_CNF[24] = 1; // Row 4
+      GPIO0->PIN_CNF[19] = 1; // Row 5
+
+      GPIO0->PIN_CNF[28] = 0; // Col 1
+      GPIO0->PIN_CNF[11] = 0; // Col 2
+      GPIO0->PIN_CNF[31] = 0; // Col 3
+      GPIO1->PIN_CNF[5] = 0;  // Col 4
+      GPIO0->PIN_CNF[30] = 0; // Col 5
+    };
+
+    /* Check if button A is pressed;
+     * turn off LED matrix if it is. */
+
+    if (ubit_button_press_a()) {
+      GPIO0->PIN_CNF[21] = 0; // Row 1
+      GPIO0->PIN_CNF[22] = 0; // Row 2
+      GPIO0->PIN_CNF[15] = 0; // Row 3
+      GPIO0->PIN_CNF[24] = 0; // Row 4
+      GPIO0->PIN_CNF[19] = 0; // Row 5
+
+      GPIO0->PIN_CNF[28] = 1; // Col 1
+      GPIO0->PIN_CNF[11] = 1; // Col 2
+      GPIO0->PIN_CNF[31] = 1; // Col 3
+      GPIO1->PIN_CNF[5] = 1;  // Col 4
+      GPIO0->PIN_CNF[30] = 1; // Col 5
+    };
+
+    sleep = 10000;
+    while (--sleep)
+      ;
+  }
+  return 0;
 }
